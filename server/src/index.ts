c0bb76b7b25b18422836aefc78d696dd7eb8c463
Rashijain2709 <rashij2709@gmail.com>
2025-07-24 01:38:13 +0530

    Some minor changes

--- server/src/index.ts
+++ server/src/index.ts
@@ -18,7 +18,7 @@ app.use('/api/engineers', engineerRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/assignments', assignmentRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 mongoose.connect(process.env.MONGO_URI!)
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
